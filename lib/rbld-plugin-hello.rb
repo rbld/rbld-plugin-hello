2054bee2f3bcce75a131eb885df9eafb222abac0
Dmitry Fleytman <dmitry@daynix.com>
2017-04-04 12:29:46 +0300

    plugin: Generalize greeting printouts
    Signed-off-by: Dmitry Fleytman <dmitry@daynix.com>

--- lib/rbld-plugin-hello.rb
+++ lib/rbld-plugin-hello.rb
@@ -4,9 +4,17 @@ require 'plugman/plugin_base'
 module RebuildPlugins
   class HelloPlugin < Plugman::PluginBase
     def start(stream = STDOUT)
-      stream.puts
-      stream.puts 'Hello from Rebuild CLI plugin'.green
-      stream.puts
+      @stream = stream
+
+      banner( 'Hello from Rebuild CLI plugin' )
+    end
+
+    private
+
+    def banner(msg)
+      @stream.puts
+      @stream.puts msg.green
+      @stream.puts
     end
   end
 end
