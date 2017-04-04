1226e535e48d8e8e024da203086391ef975f466f
Dmitry Fleytman <dmitry@daynix.com>
2017-04-04 12:32:17 +0300

    plugin: Add handlers for known and unknown command notifications
    + corrsponding unit tests
    
    Signed-off-by: Dmitry Fleytman <dmitry@daynix.com>

--- lib/rbld-plugin-hello.rb
+++ lib/rbld-plugin-hello.rb
@@ -11,12 +11,28 @@ module RebuildPlugins
       # yield
     end
 
+    def command(cmd_name, *args)
+      banner( "Hello from Rebuild CLI plugin known command #{cmd_to_s(cmd_name, args)} handler" )
+      # yiedling means that plugin errored during this notification processing
+      # yield
+    end
+
+    def unknown_command(cmd_name, *args)
+      banner( "Hello from Rebuild CLI plugin unknown command #{cmd_to_s(cmd_name, args)} handler" )
+      # yielding means that command was processed by this plugin
+      yield
+    end
+
     private
 
     def banner(msg)
       @stream.puts
       @stream.puts msg.green
       @stream.puts
+    end
+
+    def cmd_to_s(name, args)
+      "#{name}(#{args.join( ', ' )})"
     end
   end
 end
--- spec/rbld-plugin-hello_spec.rb
+++ spec/rbld-plugin-hello_spec.rb
@@ -16,6 +16,28 @@ describe "HelloPlugin" do
     expect(stream).to receive(:puts).with(/Hello/)
     @plugman.notify(:start, stream)
   end
+
+  it 'prints greeting on known command' do
+    allow(stream).to receive(:puts)
+    @plugman.notify(:start, stream)
+    regex = Regexp.escape('known command test_command(test_arg1, test_arg2)')
+    expect(stream).to receive(:puts).with(Regexp.new( regex ))
+    @plugman.notify(:command, 'test_command', :test_arg1, :test_arg2)
+  end
+
+  it 'prints greeting on unknown command' do
+    allow(stream).to receive(:puts)
+    @plugman.notify(:start, stream)
+    regex = Regexp.escape('unknown command test_command(test_arg1, test_arg2)')
+    expect(stream).to receive(:puts).with(Regexp.new( regex ))
+    @plugman.notify(:unknown_command, 'test_command', :test_arg1, :test_arg2) {}
+  end
+
+  it 'yields in unknown command processing' do
+    allow(stream).to receive(:puts)
+    @plugman.notify(:start, stream)
+    expect { |b| @plugman.notify(:unknown_command, 'test_command', &b) }.to yield_control
+  end
 end
 
 end
